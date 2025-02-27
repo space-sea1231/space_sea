13d75293057feb5962366e4cccf3da0c710ca563
space_sea <minecraftyhbl@outlook.com>
2025-02-27 14:03:00 +0800

    2.27

--- c++/a.cpp
+++ c++/a.cpp
@@ -1,118 +1,27 @@
-#include <cstring>
-#include <iostream>
-#include <queue>
-#define INF 1e9
+#include<bits/stdc++.h>
 using namespace std;
-struct edge
-{
-	int v, w, next;
-} e[10005];
-struct node
-{
-	int dis, id;
-	bool operator<(const node &a) const { return dis > a.dis; }
-	node(int d, int x) { dis = d, id = x; }
-};
-int head[5005], vis[5005], t[5005];
-int cnt, n, m;
-long long h[5005], dis[5005];
-void addedge(int u, int v, int w)
-{
-	e[++cnt].v = v;
-	e[cnt].w = w;
-	e[cnt].next = head[u];
-	head[u] = cnt;
-}
-bool spfa(int s)
-{
-	queue<int> q;
-	memset(h, 63, sizeof(h));
-	h[s] = 0, vis[s] = 1;
-	q.push(s);
-	while (!q.empty())
-	{
-		int u = q.front();
-		q.pop();
-		vis[u] = 0;
-		for (int i = head[u]; i; i = e[i].next)
-		{
-			int v = e[i].v;
-			if (h[v] > h[u] + e[i].w)
-			{
-				h[v] = h[u] + e[i].w;
-				if (!vis[v])
-				{
-					vis[v] = 1;
-					q.push(v);
-					t[v]++;
-					if (t[v] == n + 1)
-						return false;
-				}
+const int N=16;
+int a,b,t[N],w[N],T[1<<N],W[1<<N],dp[1<<N];
+signed main() {
+	scanf("%d%d",&a,&b);
+	const int mx=(1<<b)-1;
+	for(int i=1;i<=b;i++) scanf("%d%d",&t[i],&w[i]);
+	for(int i=0;i<=mx;i++) {
+		for(int j=1;j<=b;j++) {
+			if(i&(1<<(j-1))) {
+				T[i]=max(T[i],t[j]);//预处理出状态i的T和W
+				W[i]+=w[j];
 			}
 		}
 	}
-	return true;
-}
-void dijkstra(int s)
-{
-	priority_queue<node> q;
-	for (int i = 1; i <= n; i++)
-		dis[i] = INF;
-	memset(vis, 0, sizeof(vis));
-	dis[s] = 0;
-	q.push(node(0, s));
-	while (!q.empty())
-	{
-		int u = q.top().id;
-		q.pop();
-		if (vis[u])
-			continue;
-		vis[u] = 1;
-		for (int i = head[u]; i; i = e[i].next)
-		{
-			int v = e[i].v;
-			if (dis[v] > dis[u] + e[i].w)
-			{
-				dis[v] = dis[u] + e[i].w;
-				if (!vis[v])
-					q.push(node(dis[v], v));
-			}
+	memset(dp,0x3f,sizeof dp);
+	dp[0]=0;//初始化dp
+	for(int i=0;i<=mx;i++) {
+		for(int j=i;;j=i&(j-1)) {//这样就可以枚举完i的所有属于它的状态j，原理就不再多说了
+			if(W[i^j]<=a) dp[i]=min(dp[i],dp[j]+T[i^j]);
+			if(!j) break;
 		}
 	}
-	return;
-}
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	for (int i = 1; i <= m; i++)
-	{
-		int u, v, w;
-		cin >> u >> v >> w;
-		addedge(u, v, w);
-	}
-	for (int i = 1; i <= n; i++)
-		addedge(0, i, 0);
-	if (!spfa(0))
-	{
-		cout << -1 << endl;
-		return 0;
-	}
-	for (int u = 1; u <= n; u++)
-		for (int i = head[u]; i; i = e[i].next)
-			e[i].w += h[u] - h[e[i].v];
-	for (int i = 1; i <= n; i++)
-	{
-		dijkstra(i);
-		long long ans = 0;
-		for (int j = 1; j <= n; j++)
-		{
-			if (dis[j] == INF)
-				ans += j * INF;
-			else
-				ans += j * (dis[j] + h[j] - h[i]);
-		}
-		cout << ans << endl;
-	}
+	printf("%d",dp[mx]);//dp[max]即为所求
 	return 0;
 }

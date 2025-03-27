4e3e5d1eb76922fb04460644f41a1cbada892c09
space_sea <minecraftyhbl@outlook.com>
2025-03-27 18:00:41 +0800

    3.27 18:00 Room-2 submit

--- c++/std-linux/std.cpp
+++ c++/std-linux/std.cpp
@@ -1,42 +1,101 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 const int N = 1e5 + 10;
+const int K = 33;
 int n, m;
-int cnt;
-int head[N], nxt[N << 1], to[N << 1];
-void Add(int u, int v) {
+int ans, cnt;
+int head[N], to[N << 1], nxt[N << 1], val[N << 1];
+bool vis[N];
+void Add(int u, int v, int w) {
     to[++cnt] = v;
+    val[cnt] = w;
     nxt[cnt] = head[u];
     head[u] = cnt;
 }
-class Least_common_ancestors{
-    static const int K = 33;
-    int dep[N], dp[N][K];
+int dfn[N];
+struct Least_Common_Ancestors {
+    int num;
+    int dep[N], dis[N], dp[N][K];
     void Dfs(int u, int fa) {
+        dfn[u] = ++num;
         for (int i = head[u]; i; i = nxt[i]) {
-            int v = to[i];
+            int v = to[i], w = val[i];
             if (v == fa) continue;
             dep[v] = dep[u] + 1;
+            dis[v] = dis[u] + w;
             dp[v][0] = u;
-            for (int i = 1; i < K; i++) {
-                dp[u][i] = dp[dp[u][i-1]][i-1];
+            for (int j = 1; j < K; j++) {
+                dp[v][j] = dp[dp[v][j - 1]][j - 1];
             }
             Dfs(v, u);
         }
     }
-    Least_common_ancestors() {
+    int Lca(int x, int y) {
+        if (dep[x] < dep[y]) {
+            swap(x, y);
+        }
+        int srcx = x, srcy = y;
+        for (int i = K - 1; i >= 0; i--) {
+            if (dep[dp[x][i]] >= dep[y]) {
+                x = dp[x][i];
+            }
+        }
+        if (x == y) return dis[srcx] - dis[srcy];
+        for (int i = K - 1; i >= 0; i--) {
+            if (dp[x][i] != dp[y][i]) {
+                x = dp[x][i], y = dp[y][i];
+            }
+        }
+        return dis[srcx] + dis[srcy] - (dis[dp[x][0]] << 1);
+    }
+    Least_Common_Ancestors() {
         dep[0] = -1;
     }
 } Tree;
-int main() {
+struct Cmp {
+    bool operator()(int a, int b) {
+        return dfn[a] < dfn[b];
+    }
+};
+set <int, Cmp> s;
+signed main(){
     ios::sync_with_stdio(0);
     cin.tie();
     cin >> n >> m;
     for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        Add(u, v), Add(v, u);
+        int u, v, w;
+        cin >> u >> v >> w;
+        Add(u, v, w), Add(v, u, w);
     }
-    for (int i = 1; i <= m; i++)
+    Tree.Dfs(1, 0);
+    for (int i = 1; i <= m; i++) {
+        int x;
+        cin >> x;
+        if (s.empty() || s.find(x) == s.end()) {
+            int x;
+            cin >> x;
+            auto now = s.insert(x).first;
+            auto pre = prev(now), nxt = next(now);
+            if (now == s.begin()) pre = prev(s.end());
+            if (now == prev(s.end())) nxt = s.begin();
+            ans -= Tree.Lca(*pre, *nxt);
+            ans += Tree.Lca(*pre, x);
+            ans += Tree.Lca(x, *nxt);
+        } else {
+            int x;
+            cin >> x;
+            auto now = s.lower_bound(x);
+            auto pre = prev(now), nxt = next(now);
+            if (now == s.begin()) pre = prev(s.end());
+            if (now == prev(s.end())) nxt = s.begin();
+            ans += Tree.Lca(*pre, *nxt);
+            ans -= Tree.Lca(*pre, x);
+            ans -= Tree.Lca(x, *nxt);
+            s.erase(x);
+        }
+        printf("%lld\n", ans);
+    }
+
     return 0;
 }

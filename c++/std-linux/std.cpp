84c601b26a5a150d076eb24de9f54020e995cb0b
space_sea <minecraftyhbl@outlook.com>
2025-03-28 22:00:25 +0800

    3.28 22:00 Room-0 submit-2

--- c++/std-linux/std.cpp
+++ c++/std-linux/std.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 3e5 + 10;
+const int INF = 0x3f3f3f3f;
 int n, m;
-int cnt;
-int sum[N];
-int head[N], to[N << 1], nxt[N << 1], val[N << 1];
+int cnt, tot, maxn, maxx;
+int a[N], b[N], dis1[N];
+int head[N], nxt[N << 1], to[N << 1], val[N << 1];
 void Add(int u, int v, int w) {
     to[++cnt] = v;
     val[cnt] = w;
@@ -14,33 +15,64 @@ void Add(int u, int v, int w) {
 class Least_Common_Ancestors {
     static const int K = 33;
     public:
-        int dep[N], dp[N][K];
+        int dep[N], dis[N], dp[N][K], sum[N];
         void Dfs(int u, int fa) {
             for (int i = head[u]; i; i = nxt[i]) {
-                int v = to[i];
+                int v = to[i], w = val[i];
                 if (v == fa) continue;
                 dep[v] = dep[u] + 1;
+                dis[v] = dis[u] + w;
                 dp[v][0] = u;
-                for (int j = 1; j < K; j++) {
-                    dp[v][j] = dp[dp[v][j - 1]][j - 1];
+                for (int i = 1; i < K; i ++) {
+                    dp[v][i] = dp[dp[v][i - 1]][i - 1];
                 }
                 Dfs(v, u);
             }
         }
         int Lca(int x, int y) {
             if (dep[x] < dep[y]) swap(x, y);
-            for (int i = K - 1; i >= 0; i--) {
+            int srcx = x, srcy = y;
+            for (int i = K - 1; i >= 0; i --) {
                 if (dep[dp[x][i]] >= dep[y]) {
                     x = dp[x][i];
                 }
             }
             if (x == y) return x;
-            for (int i = K - 1; i >= 0; i--) {
+            for (int i = K - 1; i >= 0; i --) {
                 if (dp[x][i] != dp[y][i]) {
                     x = dp[x][i], y = dp[y][i];
                 }
             }
             return dp[x][0];
+        }
+        int Dist(int x, int y) {
+            if (dep[x] < dep[y]) swap(x, y);
+            int srcx = x, srcy = y;
+            for (int i = K - 1; i >= 0; i --) {
+                if (dep[dp[x][i]] >= dep[y]) {
+                    x = dp[x][i];
+                }
+            }
+            if (x == y) return dis[srcx] - dis[srcy];
+            for (int i = K - 1; i >= 0; i --) {
+                if (dp[x][i] != dp[y][i]) {
+                    x = dp[x][i], y = dp[y][i];
+                }
+            }
+            return dis[srcx] + dis[srcy] - (dis[dp[x][0]] << 1);
+        }
+        void Update(int u, int fa) {
+            for (int i = head[u]; i; i = nxt[i]) {
+                int v = to[i], w = val[i];
+                if (v == fa) continue;
+                Update(v, u);
+                sum[u] += sum[v];
+                if (sum[v] == tot) maxn = max(maxn, w);
+            }
+        }
+        void Init() {
+            tot = 0;
+            memset(sum, 0, sizeof(sum));
         }
     Least_Common_Ancestors() {
         dep[0] = -1;
@@ -50,16 +82,38 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie();
     cin >> n >> m;
-    for (int i = 1; i <= n; i ++) {
-        int u, v. w;
+    for (int i = 1; i < n; i ++) {
+        int u, v, w;
         cin >> u >> v >> w;
         Add(u, v, w), Add(v, u, w);
     }
     Tree.Dfs(1, 0);
     for (int i = 1; i <= m; i ++) {
-        int x, y;
-        cin >> x >> y;
-        sum[x]++, sum[y]++, sum[Tree.Lca(x, y)];
+        cin >> a[i] >> b[i];
+        dis1[i] = Tree.Dist(a[i], b[i]);
+        maxx = max(maxx, dis1[i]);
     }
+    int l = 1, r = INF, ans;
+    while (l <= r) {
+        Tree.Init();
+        int mid = (l + r) >> 1;
+        for (int i = 1; i <= m; i ++) {
+            if (dis1[i] > mid) {
+                Tree.sum[a[i]]++;
+                Tree.sum[b[i]]++;
+                Tree.sum[Tree.Lca(a[i], b[i])] -= 2;
+                tot++;
+            }
+        }
+        Tree.Update(1, 0);
+        if (maxx - maxn <= mid) {
+            ans = min(ans, mid);
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    printf("%d\n", ans);
+    
     return 0;
 }

6068bef20c012116dc49eaed91decea9e608fae0
space_sea <minecraftyhbl@outlook.com>
2025-03-28 21:03:02 +0800

    3.28 21:03 Room-0 submit

--- c++/std-linux/std.cpp
+++ c++/std-linux/std.cpp
@@ -1,101 +1,65 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
-const int N = 1e5 + 10;
-const int K = 33;
+const int N = 3e5 + 10;
 int n, m;
-int ans, cnt;
+int cnt;
+int sum[N];
 int head[N], to[N << 1], nxt[N << 1], val[N << 1];
-bool vis[N];
 void Add(int u, int v, int w) {
     to[++cnt] = v;
     val[cnt] = w;
     nxt[cnt] = head[u];
     head[u] = cnt;
 }
-int dfn[N];
-struct Least_Common_Ancestors {
-    int num;
-    int dep[N], dis[N], dp[N][K];
-    void Dfs(int u, int fa) {
-        dfn[u] = ++num;
-        for (int i = head[u]; i; i = nxt[i]) {
-            int v = to[i], w = val[i];
-            if (v == fa) continue;
-            dep[v] = dep[u] + 1;
-            dis[v] = dis[u] + w;
-            dp[v][0] = u;
-            for (int j = 1; j < K; j++) {
-                dp[v][j] = dp[dp[v][j - 1]][j - 1];
-            }
-            Dfs(v, u);
-        }
-    }
-    int Lca(int x, int y) {
-        if (dep[x] < dep[y]) {
-            swap(x, y);
-        }
-        int srcx = x, srcy = y;
-        for (int i = K - 1; i >= 0; i--) {
-            if (dep[dp[x][i]] >= dep[y]) {
-                x = dp[x][i];
+class Least_Common_Ancestors {
+    static const int K = 33;
+    public:
+        int dep[N], dp[N][K];
+        void Dfs(int u, int fa) {
+            for (int i = head[u]; i; i = nxt[i]) {
+                int v = to[i];
+                if (v == fa) continue;
+                dep[v] = dep[u] + 1;
+                dp[v][0] = u;
+                for (int j = 1; j < K; j++) {
+                    dp[v][j] = dp[dp[v][j - 1]][j - 1];
+                }
+                Dfs(v, u);
             }
         }
-        if (x == y) return dis[srcx] - dis[srcy];
-        for (int i = K - 1; i >= 0; i--) {
-            if (dp[x][i] != dp[y][i]) {
-                x = dp[x][i], y = dp[y][i];
+        int Lca(int x, int y) {
+            if (dep[x] < dep[y]) swap(x, y);
+            for (int i = K - 1; i >= 0; i--) {
+                if (dep[dp[x][i]] >= dep[y]) {
+                    x = dp[x][i];
+                }
             }
+            if (x == y) return x;
+            for (int i = K - 1; i >= 0; i--) {
+                if (dp[x][i] != dp[y][i]) {
+                    x = dp[x][i], y = dp[y][i];
+                }
+            }
+            return dp[x][0];
         }
-        return dis[srcx] + dis[srcy] - (dis[dp[x][0]] << 1);
-    }
     Least_Common_Ancestors() {
         dep[0] = -1;
     }
 } Tree;
-struct Cmp {
-    bool operator()(int a, int b) {
-        return dfn[a] < dfn[b];
-    }
-};
-set <int, Cmp> s;
-signed main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie();
     cin >> n >> m;
-    for (int i = 1; i < n; i++) {
-        int u, v, w;
+    for (int i = 1; i <= n; i ++) {
+        int u, v. w;
         cin >> u >> v >> w;
         Add(u, v, w), Add(v, u, w);
     }
     Tree.Dfs(1, 0);
-    for (int i = 1; i <= m; i++) {
-        int x;
-        cin >> x;
-        if (s.empty() || s.find(x) == s.end()) {
-            int x;
-            cin >> x;
-            auto now = s.insert(x).first;
-            auto pre = prev(now), nxt = next(now);
-            if (now == s.begin()) pre = prev(s.end());
-            if (now == prev(s.end())) nxt = s.begin();
-            ans -= Tree.Lca(*pre, *nxt);
-            ans += Tree.Lca(*pre, x);
-            ans += Tree.Lca(x, *nxt);
-        } else {
-            int x;
-            cin >> x;
-            auto now = s.lower_bound(x);
-            auto pre = prev(now), nxt = next(now);
-            if (now == s.begin()) pre = prev(s.end());
-            if (now == prev(s.end())) nxt = s.begin();
-            ans += Tree.Lca(*pre, *nxt);
-            ans -= Tree.Lca(*pre, x);
-            ans -= Tree.Lca(x, *nxt);
-            s.erase(x);
-        }
-        printf("%lld\n", ans);
+    for (int i = 1; i <= m; i ++) {
+        int x, y;
+        cin >> x >> y;
+        sum[x]++, sum[y]++, sum[Tree.Lca(x, y)];
     }
-
     return 0;
 }

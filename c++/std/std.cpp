4d57b1b777dbcabaaad1a5c97241fb8945d10a5e
space_sea <minecraftyhbl@outlook.com>
2025-07-05 09:32:23 +0800

    7.5 9:32 Room-3 submit-2

--- c++/std/std.cpp
+++ c++/std/std.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
 const int N=1e5+10;
 
 int n;
+int valx[N<<1];
 
 struct ScanLine{
     int l, r, h;
@@ -14,7 +16,10 @@ struct ScanLine{
     bool operator<(const ScanLine &ref)const{
         return h<ref.h;
     }
-}line;
+}line[N<<1];
+struct SegmentTree{
+    
+}
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -23,7 +28,18 @@ signed main(){
     for (int i=1; i<=n; i++){
         int sx, sy, fx, fy;
         cin >> sx >> sy >> fx >> fy;
-        
+        line[(i<<1)-1]=(ScanLine){sx, fx, sy, 1};
+        line[i<<1]=(ScanLine){sx, fx, fy, -1};
+        valx[(i<<1)-1]=sx;
+        valx[i<<1]=fx;
+    }
+    n<<=1;
+    sort(line+1, line+n+1);
+    sort(valx+1, valx+n+1);
+    int tot=unique(valx+1, valx+n+1)-valx-1;
+    seg.Init(1, 1, tot-1);
+    for (int i=1; i<n; i++){
+        seg.Add(1, line[i].l, line[i].r, line[i].val);
     }
     return 0;
 }

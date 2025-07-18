2510b26efba898bcf4e5c14d5a7f1d384d6d9104
space_sea <minecraftyhbl@outlook.com>
2025-07-18 09:01:49 +0800

    7.18 9:01 Room-3 submit-1

--- c++/std/std.cpp
+++ c++/std/std.cpp
@@ -1,62 +1,45 @@
 #include <iostream>
 #include <stdio.h>
-#define Mod(x) (x%MOD+MOD)%MOD
 #define __Debug
 
 using namespace std;
 typedef long long ll;
-typedef __int128 i128;
 
-const int N=10;
-const int MOD=1e9+7;
+const int N=1e6+10;
+const int M=70;
 
-int t, n;
-i128 a[N][N], sum[N][N], tmp[N][N];
+ll t, n;
+int a[N];
+int cnt[M], sum[N];
 
 void Init(){
-    for (int i=1; i<=2; i++){
-        for (int j=1; j<=2; j++){
-            sum[i][j]=a[i][j]=0;
-        }
+    for (int i=1; i<=n; i++){
+        sum[i]=0;
     }
-    for (int i=1; i<=2; i++){
-        sum[i][i]=1;
-    }
-    a[1][1]=a[1][2]=a[2][1]=1;
-    return;
-}
-void Init1(){
-    for (int i=1; i<=2; i++){
-        for (int j=1; j<=2; j++){
-            tmp[i][j]=0;
-        }
+    for (int i=1; i<=64; i++){
+        cnt[i]=0;
     }
     return;
 }
-void Pow1(i128 a[][N], i128 b[][N]){
-    Init1();
-    for (int i=1; i<=2; i++){
-        for (int j=1; j<=2; j++){
-            for (int k=1; k<=2; k++){
-                tmp[i][j]=Mod(tmp[i][j]+a[i][k]*b[k][j]);
-            }
+void Decompose(int a){
+    int now=1;
+    while (a>0){
+        if (a&1==true){
+            cnt[now]++;
         }
-    }
-    for (int i=1; i<=2; i++){
-        for (int j=1; j<=2; j++){
-            a[i][j]=tmp[i][j];
-        }
+        now++;
+        a=a>>1;
     }
     return;
 }
-void Pow(){
-    Init();
-    while (n>0){
-        if (n&1==true){
-            Pow1(sum, a);
+void Decompose1(int a, int id){
+    int now=1;
+    while (a>0){
+        if (a&1==true&&cnt[now]%2==true){
+            sum[id]=sum[id]+(1<<(now-1));
         }
-        Pow1(a, a);
-        n=n>>1;
+        now++;
+        a=a>>1;
     }
     return;
 }
@@ -64,12 +47,40 @@ signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> n;
-    // if (n<=3){
-    //     printf("1\n");
-    //     continue;
-    // }
-    Pow();
-    printf("%lld\n", (ll)sum[2][1]);
+    cin >> t;
+    while (t--){
+        Init();
+        cin >> n;
+        for (int i=1; i<=n; i++){
+            cin >> a[i];
+            Decompose(a[i]);
+        }
+        for (int i=1; i<=n; i++){
+            Decompose1(a[i], i);
+        }
+        int ans=0;
+        for (int i=1; i<=64; i++){
+            if (cnt[i]%2==1){
+                ans=ans+(1<<(i-1));
+            }
+            #ifdef __Debug
+                printf("%d ", cnt[i]);
+            #endif
+        }
+        #ifdef __Debug
+            printf("\n");
+        #endif
+        printf("%d\n", ans);
+    }
     return 0;
 }
+/*
+0110
+0101
+0111
+
+0101
+1001
+4+8=12
+4+0=4
+*/

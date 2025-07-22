9b2a1f9071f456a58e07b2f8fc3057b19a35d132
space_sea <minecraftyhbl@outlook.com>
2025-07-22 17:19:56 +0800

    7.22 17:19 Room-3 Submit-1

--- c++/a.cpp
+++ c++/a.cpp
@@ -1,92 +1,105 @@
+#include <algorithm>
 #include <iostream>
-#include <stdio.h>
-#define __Debug
 
-using namespace std;
-typedef long long ll;
-
-const int N=1e6+10;
+constexpr int MAXN = 1e5 + 10;
+constexpr int MAXK = 2e5 + 10;
 
 int n, m;
-int a[N];
 
-namespace SegmentTree{
-    int num;
-    int root[N<<1];
-    
-    struct Node{
-        int lson, rson;
-        int dat;
-    };
-    Node node[N<<8];
+struct Element {
+  int a, b, c;
+  int cnt;
+  int res;
 
-    int Build(int l, int r){
-        int p=++num;
-        if (l==r){
-            node[p].dat=a[l];
-            return p;
-        }
-        int mid=(l+r)>>1;
-        node[p].lson=Build(l, mid);
-        node[p].rson=Build(mid+1, r);
-        return p;
+  bool operator!=(Element other) const {
+    if (a != other.a) return true;
+    if (b != other.b) return true;
+    if (c != other.c) return true;
+    return false;
+  }
+};
+
+Element e[MAXN];
+Element ue[MAXN];
+int m, t;
+int res[MAXN];
+
+struct BinaryIndexedTree {
+  int node[MAXK];
+
+  int lowbit(int x) { return x & -x; }
+
+  void Add(int pos, int val) {
+    while (pos <= m) {
+      node[pos] += val;
+      pos += lowbit(pos);
     }
-    int Insert(int now, int l, int r, int cur, int val){
-        int p=++num;
-        node[p]=node[now];
-        if (l==r){
-            node[p].dat=val;
-            return p;
-        }
-        int mid=(l+r)>>1;
-        if (cur<=mid){
-            node[p].lson=Insert(node[now].lson, l, mid, cur, val);
-        }
-        else{
-            node[p].rson=Insert(node[now].rson, mid+1, r, cur, val);
-        }
-        return p;
+    return;
+  }
+
+  int Ask(int pos) {
+    int res = 0;
+    while (pos) {
+      res += node[pos];
+      pos -= lowbit(pos);
     }
-    int Query(int now, int l, int r, int cur){
-        if (l==r){
-            return node[now].dat;
-        }
-        int mid=(l+r)>>1;
-        if (cur<=mid){
-            return Query(node[now].lson, l, mid, cur);
-        }
-        else{
-            return Query(node[now].rson, mid+1, r, cur);
-        }
-        return 0;
-    }
+    return res;
+  }
+} BIT;
+
+bool cmpA(Element x, Element y) {
+  if (x.a != y.a) return x.a < y.a;
+  if (x.b != y.b) return x.b < y.b;
+  return x.c < y.c;
 }
-using namespace SegmentTree;
 
-signed main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+bool cmpB(Element x, Element y) {
+  if (x.b != y.b) return x.b < y.b;
+  return x.c < y.c;
+}
 
-    cin >> n >> m;
-    for (int i=1; i<=n; i++){
-        cin >> a[i];
+void CDQ(int l, int r) {
+  if (l == r) return;
+  int mid = (l + r) / 2;
+  CDQ(l, mid);
+  CDQ(mid + 1, r);
+  std::sort(ue + l, ue + mid + 1, cmpB);
+  std::sort(ue + mid + 1, ue + r + 1, cmpB);
+  int i = l;
+  int j = mid + 1;
+  while (j <= r) {
+    while (i <= mid && ue[i].b <= ue[j].b) {
+      BIT.Add(ue[i].c, ue[i].cnt);
+      i++;
     }
-    root[0]=Build(1, n);
-    for (int i=1; i<=m; i++){
-        int ver, opt;
-        cin >> ver >> opt;
-        if (opt==1){
-            int cur, val;
-            cin >> cur >> val;
-            root[i]=Insert(root[ver], 1, n, cur, val);
-        }
-        if (opt==2){
-            int cur;
-            cin >> cur;
-            printf("%d\n", Query(root[ver], 1, n, cur));
-            root[i]=root[ver];
-        }
-    }
+    ue[j].res += BIT.Ask(ue[j].c);
+    j++;
+  }
+  for (int k = l; k < i; k++) BIT.Add(ue[k].c, -ue[k].cnt);
+  return;
+}
 
-    return 0;
+using std::cin;
+using std::cout;
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) cin >> e[i].a >> e[i].b >> e[i].c;
+  std::sort(e + 1, e + n + 1, cmpA);
+  for (int i = 1; i <= n; i++) {
+    t++;
+    if (e[i] != e[i + 1]) {
+      m++;
+      ue[m].a = e[i].a;
+      ue[m].b = e[i].b;
+      ue[m].c = e[i].c;
+      ue[m].cnt = t;
+      t = 0;
+    }
+  }
+  CDQ(1, m);
+  for (int i = 1; i <= m; i++) res[ue[i].res + ue[i].cnt - 1] += ue[i].cnt;
+  for (int i = 0; i < n; i++) cout << res[i] << '\n';
+  return 0;
 }

f867b60a9e6a4dc1961a0dcd6a2ba42878f57526
space_sea <minecraftyhbl@outlook.com>
2025-07-24 17:28:16 +0800

    7.24 17:28 Room-3 Submit-3

--- c++/c.cpp
+++ c++/c.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
-#include <vector>
+#include <stdio.h>
 #define __Debug
 
 using namespace std;
 typedef long long ll;
-vector<int> v;
+
+const int N=2e5+10;
+
+int t;
+int n;
+int a[N];
+int f[N];
+
 signed main(){
-  v.reserve(1e6);
-    v.push_back(1);
-    for(int i:v){
-      v.push_back(i);
+  cin.tie(nullptr)->ios::sync_with_stdio(false);
+  cin >> t;
+  while (t--){
+    cin >> n;
+    /*Input*/
+    for (int i=1; i<=n; i++){
+      cin >> a[i];
+    } 
+    /*Init*/
+    for (int i=1; i<=n; i++){
+      f[i]=0;
     }
-    return 0;
+    /*Solve*/
+    int ans=0;
+    for (int i=1; i<=n; i++){
+      if (a[i]+i<=n){
+        f[i+a[i]]=max(f[i+a[i]], f[i]+a[i]);
+      }
+      f[i]=f[i]+a[i];
+      ans=max(ans, f[i]);
+    }
+    printf("%d\n", ans);
+  }
+  return 0;
 }

fdf9a0a6ca165a2ca36fd0a029b4ca1ad8e95bfd
space_sea <minecraftyhbl@outlook.com>
2025-07-28 14:14:44 +0800

    7.28 14:14 Room-3 Submit-2

--- c++/c.cpp
+++ c++/c.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 #include <stdio.h>
 #define __Debug
-
+#define Mod(x) (x % MOD + MOD) % MOD
 using namespace std;
 typedef long long ll;
 
-const int N=2e5+10;
+const int MOD = 1e9 + 7;
 
-int t;
-int n;
-int a[N];
-int f[N];
-
-signed main(){
-  cin.tie(nullptr)->ios::sync_with_stdio(false);
-  cin >> t;
-  while (t--){
-    cin >> n;
-    /*Input*/
-    for (int i=1; i<=n; i++){
-      cin >> a[i];
-    } 
-    /*Init*/
-    for (int i=1; i<=n; i++){
-      f[i]=0;
-    }
-    /*Solve*/
-    int ans=0;
-    for (int i=1; i<=n; i++){
-      if (a[i]+i<=n){
-        f[i+a[i]]=max(f[i+a[i]], f[i]+a[i]);
-      }
-      f[i]=f[i]+a[i];
-      ans=max(ans, f[i]);
-    }
-    printf("%d\n", ans);
+int n, m;
+int sum;
+int Pow(int a, int b) {
+  int sum = 1;
+  while (b > 0) {
+    if (b & 1 == 1) sum = Mod((ll)sum * a);
+    a = Mod((ll)a * a);
+    b = b >> 1;
   }
+  return sum;
+}
+
+signed main() {
+  cin.tie(nullptr) -> ios::sync_with_stdio(false);
+  /*Input*/
+  cin >> n >> m;
+  /*Init*/
+  sum = Mod(Pow(2, n) - 1);
+  int ans = 1;
+  /*Solve*/
+  for (int i = 0; i < n; i++) {
+    int res=Mod(Pow(sum, m)-Pow(Mod(sum+Pow(2, i)), m));
+    ans=Mod(ans*Mod(Pow(sum, Mod-2)));
+  }
+  printf("%d\n", ans);
+  /*Output*/
   return 0;
 }
--- c++/std/std.cpp
+++ c++/std/std.cpp
@@ -4,9 +4,27 @@
 
 using namespace std;
 typedef long long ll;
-
-signed main(){
+const int N=1e6+10;
+int n, m;
+struct Node {
+  int val;
+  int id;
+  bool operator<(const Node srcb) const{
+    return val>srcb.val;
+  }
+}a[N], b[N];
+signed main() {
   cin.tie(nullptr) -> ios::sync_with_stdio(false);
-
+  cin >> n >> m;
+  for (int i = 1; i<=n; i++){
+    cin >> a[i].val;
+    a[i].id=i;
+  }
+  for (int j = 1; j <= n; j++) {
+    cin >> b[i].val;
+    b[i].id=i;
+    b[i].val+=a[i].val;
+  }
+  
   return 0;
 }

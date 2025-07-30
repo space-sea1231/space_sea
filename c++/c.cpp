8713ded9a359b624101f0d299e42dfad5059cd19
space_sea <minecraftyhbl@outlook.com>
2025-07-30 09:10:26 +0800

    7.30 9.10 Room-3 Submit-2

--- c++/c.cpp
+++ c++/c.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
 #include <stdio.h>
 #define __Debug
-#define Mod(x) (x % MOD + MOD) % MOD
+
 using namespace std;
 typedef long long ll;
 
-const int MOD = 1e9 + 7;
+const int N = 8e3 + 10;
 
-int n, m;
-int sum;
-int Pow(int a, int b) {
-  int sum = 1;
-  while (b > 0) {
-    if (b & 1 == 1) sum = Mod((ll)sum * a);
-    a = Mod((ll)a * a);
-    b = b >> 1;
+int n;
+
+struct Binary {
+  int sum[N];
+
+  int Lowbit(int x) { return x&-x; }
+
+  void Add(int pos, int val) {
+    for (int i = pos; i <= n; i += Lowbit(i)) sum[i] += val;
+    return;
   }
-  return sum;
-}
+
+  int Query(int pos) {
+    int ans = 0;
+    for (int i = pos; i > 0; i -= Lowbit(i)) ans += sum[i];
+    return ans;
+  }
+};
+
+int a[N];
+int ans[N];
+Binary bin;
 
 signed main() {
   cin.tie(nullptr) -> ios::sync_with_stdio(false);
   /*Input*/
-  cin >> n >> m;
+  cin >> n;
+  for (int i = 2; i <= n; i++) cin >> a[i];
   /*Init*/
-  sum = Mod(Pow(2, n) - 1);
-  int ans = 1;
+  for (int i = 1; i <= n; i++) bin.Add(i, 1);
   /*Solve*/
-  for (int i = 0; i < n; i++) {
-    int res=Mod(Pow(sum, m)-Pow(Mod(sum+Pow(2, i)), m));
-    ans=Mod(ans*Mod(Pow(sum, Mod-2)));
+  for (int i = n; i >= 1; i--) {
+    int l = 1, r = n;
+    while (l <= r) {
+      int mid = (l+r)>>1;
+      if (bin.Query(mid) < a[i]) l = mid+1;
+      else r = mid-1;
+    }
+    ans[i] = r;
+    bin.Add(r, -1);
   }
-  printf("%d\n", ans);
   /*Output*/
+  for (int i = 1; i <= n; i++) printf("%d\n", ans[i]);
   return 0;
 }
--- c++/std/std.cpp
+++ c++/std/std.cpp
@@ -5,50 +5,18 @@
 using namespace std;
 typedef long long ll;
 
-const int N = 8e3 + 10;
-
 int n;
-
-struct Binary {
-  int sum[N];
-
-  int Lowbit(int x) { return x&-x; }
-
-  void Add(int pos, int val) {
-    for (int i = pos; i <= n; i += Lowbit(i)) sum[i] += val;
-    return;
-  }
-
-  int Query(int pos) {
-    int ans = 0;
-    for (int i = pos; i > 0; i -= Lowbit(i)) ans += sum[i];
-    return ans;
-  }
-};
-
-int a[N];
-int ans[N];
-Binary bin;
 
 signed main() {
   cin.tie(nullptr) -> ios::sync_with_stdio(false);
   /*Input*/
   cin >> n;
-  for (int i = 2; i <= n; i++) cin >> a[i];
-  /*Init*/
-  for (int i = 1; i <= n; i++) bin.Add(i, 1);
-  /*Solve*/
-  for (int i = n; i >= 1; i--) {
-    int l = 1, r = n;
-    while (l <= r) {
-      int mid = (l+r)>>1;
-      if (bin.Query(mid) < a[i]) l = mid+1;
-      else r = mid-1;
-    }
-    ans[i] = r;
-    bin.Add(r, -1);
+  if (n <= 15) {
+    using namespace Pts1;
+    
   }
+  /*Init*/
+  /*Solve*/
   /*Output*/
-  for (int i = 1; i <= n; i++) printf("%d\n", ans[i]);
   return 0;
 }
